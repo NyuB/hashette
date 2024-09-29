47a549977634f6d8c67f77460e86bcee9f0ea691
Brice Decaestecker <brice.decaestecker@gmx.fr>
2024-09-29 12:36:08 +0200

    Implement grouping functionnality back

--- hashette.ml
+++ hashette.ml
@@ -7,6 +7,7 @@ module FileSystem : sig
     | Dir of dir
 
   val path_as_file : string -> t option
+  val file_as_path : t -> string
   val read_bytes : file -> bytes
   val children : dir -> (string * t) list
 end = struct
@@ -25,6 +26,10 @@ end = struct
     else if Sys.is_regular_file path
     then Some (File path)
     else None
+  ;;
+
+  let file_as_path = function
+    | File p | Dir p -> p
   ;;
 
   let children dir =
@@ -57,36 +62,119 @@ open FileSystem
 
 let compare_child_entries (a, _) (b, _) = String.compare a b
 
-module Hashette (H : Digestif.S) = struct
+module StringMap = Hashtbl.Make (String)
+module StringSet = Set.Make (String)
+
+module Record = struct
+  module type T = sig
+    type t
+    type h
+
+    val record : t -> h -> string -> unit
+    val create : unit -> t
+  end
+
+  module Make (H : Digestif.S) :
+    T with type h = H.t and type t = StringSet.t StringMap.t = struct
+    type t = StringSet.t StringMap.t
+    type h = H.t
+
+    let record (t : t) (h : H.t) (path : string) : unit =
+      let key = H.to_hex h in
+      match StringMap.find_opt t key with
+      | None -> StringMap.add t key (StringSet.singleton path)
+      | Some set -> StringMap.replace t key (StringSet.add path set)
+    ;;
+
+    let create () : t = StringMap.create 50
+  end
+
+  module None (Any : sig
+      type t
+    end) : T with type h = Any.t and type t = unit = struct
+    type t = unit
+    type h = Any.t
+
+    let record () _ _ = ()
+    let create () = ()
+  end
+end
+
+module Hashette (H : Digestif.S) (R : Record.T with type h = H.t) = struct
   type h = H.t
 
   let _to_hex_uppercase h = String.uppercase_ascii (H.to_hex h)
   let to_hex_lowercase h = String.lowercase_ascii (H.to_hex h)
   let hash_single_file f : h = H.digest_bytes (FileSystem.read_bytes f)
 
-  let rec hash_path (file_or_dir : FileSystem.t) : h =
+  let rec hash_path (r : R.t) (file_or_dir : FileSystem.t) : h =
+    let recorded v =
+      let () = R.record r v (file_as_path file_or_dir) in
+      v
+    in
     match file_or_dir with
-    | File f -> hash_single_file f
+    | File f -> recorded @@ hash_single_file f
     | Dir d ->
       children d
       |> List.sort compare_child_entries
       |> List.fold_left
            (fun hctx (name, f) ->
              let h_name = H.feed_string hctx name in
-             hash_path f |> H.to_raw_string |> H.feed_string h_name)
+             hash_path r f |> H.to_raw_string |> H.feed_string h_name)
            H.empty
       |> H.get
+      |> recorded
   ;;
 end
 
+let print_record_entry (h, paths) =
+  print_endline (Printf.sprintf "%s" h);
+  StringSet.to_seq paths |> Seq.iter (fun s -> print_endline @@ Printf.sprintf "    %s" s)
+;;
+
+let size_desc_then_string (ha, sa) (hb, sb) =
+  let ic = Int.compare (StringSet.cardinal sb) (StringSet.cardinal sa) in
+  if ic = 0 then String.compare ha hb else ic
+;;
+
+let sorted_in_place by arr =
+  let () = Array.sort by arr in
+  arr
+;;
+
+let print_record record =
+  record
+  |> StringMap.to_seq
+  |> Array.of_seq
+  |> sorted_in_place size_desc_then_string
+  |> Array.iter print_record_entry
+;;
+
+let hash file =
+  let module R = Record.None (Digestif.SHA256) in
+  let module H = Hashette (Digestif.SHA256) (R) in
+  let h = H.hash_path () file in
+  print_endline @@ H.to_hex_lowercase h
+;;
+
+let group file =
+  let module R = Record.Make (Digestif.SHA256) in
+  let module H = Hashette (Digestif.SHA256) (R) in
+  let record = R.create () in
+  let _ = H.hash_path record file in
+  print_record record
+;;
+
 let () =
-  let file_path = Sys.argv.(1) in
+  let command = Sys.argv.(1) in
+  let file_path = Sys.argv.(2) in
   let file =
     match FileSystem.path_as_file file_path with
     | None -> failwith @@ Printf.sprintf "File %s does not exist" file_path
     | Some f -> f
   in
-  let module H = Hashette (Digestif.SHA256) in
-  let h = H.hash_path file in
-  print_endline @@ H.to_hex_lowercase h
+  match command with
+  | "hash" -> hash file
+  | "group" -> group file
+  | any -> print_endline @@ Printf.sprintf "Unknown command %s" any
 ;;
--- usage.t/run.t
+++ usage.t/run.t
@@ -1,20 +1,36 @@
 Single file
-  $ hashette resources/a.txt
+  $ hashette hash resources/a.txt
   cb1ad2119d8fafb69566510ee712661f9f14b83385006ef92aec47f523a38358
 
 Missing file
-  $ hashette absent.txt
+  $ hashette hash absent.txt
   Fatal error: exception Failure("File absent.txt does not exist")
   [2]
 
 Folders with the same contents have the same hash
-  $ hashette resources/folder_a
+  $ hashette hash resources/folder_a
   7d72da89e2fc75cbc9c590648494982f1b883f11e3afa5ca0b93d06608ca24b8
-  $ hashette resources/folder_a_copy
+  $ hashette hash resources/folder_a_copy
   7d72da89e2fc75cbc9c590648494982f1b883f11e3afa5ca0b93d06608ca24b8
 
 Folders' children' names are treated differently from their content
-  $ hashette resources/tricky.txt
+  $ hashette hash resources/tricky.txt
   ccf8b2c1266d8799cc720aca6013ef370ee4ecd39ab7c36c2dd0f0e3cf4a75d3
-  $ hashette resources/tricky_folder
+  $ hashette hash resources/tricky_folder
   18c00f0264da56e2a9280636b862f4520134ed2e00af242e2add7b5376575e5b
+
+Group files by hash
+  $ hashette group resources/groups
+  cb1ad2119d8fafb69566510ee712661f9f14b83385006ef92aec47f523a38358
+      resources/groups/a.txt
+      resources/groups/b_with_a.txt
+      resources/groups/folder_one/a.txt
+      resources/groups/folder_three/a.txt
+      resources/groups/folder_two/b_with_a.txt
+  ce2ccf9dfaab921b0648f70777ed22303c0071598bd0052d24b9292d636ad203
+      resources/groups/folder_one
+      resources/groups/folder_three
+  5436b40aaaf4b4b54633058af3108ca481562764ec548c95627f032998451067
+      resources/groups
+  93d93833fd18fae952c6d42204b23c506db5725149514ad51f8d3f7ef3ad26bd
+      resources/groups/folder_two
